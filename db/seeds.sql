42b56af730c6dbacff3f52d13bbc2de64c27914e
Jonathan Bridge <jvbridge@gmail.com>
2022-04-30 15:04:16 -0700

    added some seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,8 @@
--- These are some seed values for our database using some members of the 
--- USS ENTERPRISE NCC-1701-D. There is no money in the 24th Century so they're
--- paid with units of pride and accomplishment 
+/*
+These are some seed values for our database using some members of the USS 
+ENTERPRISE NCC-1701-D. There is no money in the 24th Century so they're paid 
+with units of pride and accomplishment 
+*/
 
 INSERT INTO department (department_name)
 VALUES 
@@ -24,7 +26,7 @@ VALUES
     ("Security", 100, 
         (SELECT id FROM department WHERE department_name = "Operations")
     ),
-    ("Engineer", 100, 
+    ("Engineering", 100, 
         (SELECT id FROM department WHERE department_name = "Operations")
     ),
     ("Operations", 100, 
@@ -38,44 +40,106 @@ VALUES
     ),
     ("Science", 100, 
         (SELECT id FROM department WHERE department_name = "Science")
+    ),
+    ("Crewman", 100,
+        (SELECT id FROM department WHERE department_name = "Command")
+    ),
+    ("Civilian", 100,
+        (SELECT id FROM department WHERE department_name = "Command")
     );
 
 SELECT * FROM role;
 
 -- Need to do a new insert for each layer of management
+
+-- Picard is the big boss
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ("Jean-Luc", "Picard", (SELECT id FROM role WHERE name = "Captain"), NULL);
+    ("Jean-Luc", "Picard", (SELECT id FROM role WHERE title = "Captain"), NULL);
+
+-- Rest of the bridge crew report to him 
+/*
+This only works because nobody else is named "Picard", This behavior is 
+problematic when last names collide (such as for Beverley and Wesley Crusher).
+For this exercise I'm going to just hard code things, but ideally a more full
+implementation would reference a role they report to, and have unique roles for
+each chief. (Data is chief of ops, Beverley Crusher is chief of medical etc.)
+*/
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
     ("William", "Riker", 
-        (SELECT id FROM role WHERE name = "First Officer"), 
-        (SELECT id from employee WHERE title = "Captain")
+        (SELECT id FROM role WHERE title = "First Officer"), 
+        (SELECT * FROM 
+            (SELECT id FROM employee WHERE last_name = "Picard") as id
+        )
     ),
-    ("Data", "Soong", 
-        (SELECT id FROM role WHERE name = "Second Officer"), 
-        (SELECT id from employee WHERE title = "Captain")
+    ("Data", NULL, 
+        (SELECT id FROM role WHERE title = "Second Officer"), 
+        (SELECT * FROM 
+            (SELECT id FROM employee WHERE last_name = "Picard") as id
+        )
     ),
-    ("Worf", "Son of Mogh", 
-        (SELECT id FROM role WHERE name = "Security"), 
-        (SELECT id from employee WHERE title = "Captain")
+    ("Worf", NULL, 
+        (SELECT id FROM role WHERE title = "Security"), 
+        (SELECT * FROM 
+            (SELECT id FROM employee WHERE last_name = "Picard") as id
+        )
     ),
     ("Geordi", "La Forge", 
-        (SELECT id FROM role WHERE name = "Engineering"), 
-        (SELECT id from employee WHERE title = "Captain")
-    ),
-    ("Deanna", "Troi", 
-        (SELECT id FROM role WHERE name = "Counsilor"), 
-        (SELECT id from employee WHERE title = "Captain")
+        (SELECT id FROM role WHERE title = "Engineering"), 
+        (SELECT * FROM 
+            (SELECT id FROM employee WHERE last_name = "Picard") as id
+        )
     ),
     ("Beverly", "Crusher", 
-        (SELECT id FROM role WHERE name = "Medical"), 
-        (SELECT id from employee WHERE title = "Captain")
-    )
+        (SELECT id FROM role WHERE title = "Medical"), 
+        (SELECT * FROM 
+            (SELECT id FROM employee WHERE last_name = "Picard") as id
+        )
+    ),
+    ("Deanna", "Troi", 
+        (SELECT id FROM role WHERE title = "Counsilor"), 
+        (SELECT * FROM 
+            (SELECT id FROM employee WHERE last_name = "Picard") as id
+        )
+    );
+
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ("Reginald", "Barclay", 
-        (SELECT id FROM role WHERE name = "Engineer"), 
-        (SELECT id from employee WHERE last_name = "La Forge")
+    ("Miles", "O'Brian",
+        (SELECT id FROM role WHERE title = "Operations"),
+        (SELECT * FROM 
+            (SELECT id FROM employee WHERE first_name = "Worf") as id
+        )
     ),
-    
+    ("Reginald", "Barclay", 
+        (SELECT id FROM role WHERE title = "Engineering"), 
+        (SELECT * FROM 
+            (SELECT id FROM employee WHERE last_name = "La Forge") as id
+        )
+    ),
+    ("Alyssa", "Ogawa",
+        (SELECT id FROM role WHERE title = "Medical"), 
+        (SELECT * FROM 
+            (SELECT id FROM employee WHERE last_name = "Crusher") as id
+        )
+    ),
+    ("Wesley", "Crusher",
+        (SELECT id FROM role WHERE title = "Civilian"),
+        (SELECT * FROM 
+            (SELECT id FROM employee WHERE last_name = "Picard") as id
+        )
+    ),
+    ("Guinan", NULL,
+        (SELECT id FROM role WHERE title = "Civilian"),
+        (SELECT * FROM 
+            (SELECT id FROM employee WHERE last_name = "Picard") as id
+        )
+    ),
+    ("Ro", "Laren",
+        (SELECT id FROM role WHERE title = "Crewman"),
+        (SELECT * FROM 
+            (SELECT id FROM employee WHERE last_name = "Riker") as id
+        )
+    );
+SELECT * FROM employee;
