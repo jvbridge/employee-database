cee0656aff5627ea5695c6e983f48cb54e0f233d
Jonathan Bridge <jvbridge@gmail.com>
2022-04-29 18:58:34 -0700

    added input validation

--- test/role.test.js
+++ test/role.test.js
@@ -13,4 +13,23 @@ describe('Role', () =>{
         expect(colin.getSalary()).toEqual(salary);
         expect(colin.getDepartmentId()).toEqual(departmentId);
     });
+
+    it ('should validate input and assign default values', () =>{
+        const warn = jest.spyOn(console, "warn").mockImplementation(()=>{});
+
+        const dent = new Role(id, title, "Howdy!", departmentId);
+        expect(warn).toBeCalledWith("Invalid salary given, setting to default");
+        expect(dent.getSalary()).toEqual()
+
+
+
+        // department ID
+        const colin = new Role(id, title, salary);
+        expect(warn).toBeCalledWith("Invalid ID given, setting to default");
+        expect(colin.getDepartmentId()).toEqual(colin.getDefaultDepartment());   
+
+        const ford = new Role(id, title, salary, 1.5);
+        expect(ford.getDepartmentId()).toEqual(colin.getDepartmentId());    
+    });
+
 });
