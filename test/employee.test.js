051598059850da757f237c624e9dbde023ab03fc
Jonathan Bridge <jvbridge@gmail.com>
2022-04-30 15:27:16 -0700

    added null handling for employees

--- test/employee.test.js
+++ test/employee.test.js
@@ -7,16 +7,9 @@ describe("Employee", () => {
     const managerId = 7787;
 
     it("should have an id, first name, last name, role id, and manager id", () => {
-        const dentArthurDent = new Employee(
-            firstName,
-            lastName,
-            roleId,
-            managerId
-        );
+        const dentArthurDent = new Employee(firstName, lastName);
 
         expect(dentArthurDent.getFirstName()).toEqual(firstName);
         expect(dentArthurDent.getLastName()).toEqual(lastName);
-        expect(dentArthurDent.getRoleId()).toEqual(roleId);
-        expect(dentArthurDent.getManagerId()).toEqual(managerId);
     });
 });
